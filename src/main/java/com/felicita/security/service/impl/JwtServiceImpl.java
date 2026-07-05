2769426949245a759baece4289f4439aab1a2524
PasinduDilshan99 <pd.dimbulana@gmail.com>
2026-07-05 20:27:33 +0530

    add

--- src/main/java/com/felicita/security/service/impl/JwtServiceImpl.java
+++ src/main/java/com/felicita/security/service/impl/JwtServiceImpl.java
@@ -142,11 +142,11 @@
         public ResponseCookie buildAccessTokenCookie(String token) {
             return ResponseCookie.from(accessCookieName, token)
                     .httpOnly(true)
-                    .secure(false)
+                    .secure(true)
                     .path("/")
                     .maxAge(Duration.ofSeconds(accessCookieMaxAge))
-                    .sameSite("Strict")
-//                    .domain(".felicitatrips.com")
+                    .sameSite("None")
+                    .domain(".felicitatrips.com")
                     .build();
         }
 
@@ -154,10 +154,10 @@
         public ResponseCookie buildRefreshTokenCookie(String token) {
             return ResponseCookie.from(refreshCookieName, token)
                     .httpOnly(true)
-                    .secure(false)
+                    .secure(true)
                     .path("/")
                     .maxAge(Duration.ofSeconds(refreshCookieMaxAge))
-                    .sameSite("Strict")
+                    .sameSite("None")
 //                    .domain(".felicitatrips.com")
                     .build();
         }
@@ -213,11 +213,11 @@
         public ResponseCookie buildLogoutAccessTokenCookie() {
             return ResponseCookie.from(accessCookieName, "")
                     .httpOnly(true)
-                    .secure(false)
+                    .secure(true)
                     .path("/")
                     .maxAge(0)
-                    .sameSite("Strict")
-//                    .domain(".felicitatrips.com")
+                    .sameSite("None")
+                    .domain(".felicitatrips.com")
                     .build();
         }
 
@@ -225,11 +225,11 @@
         public ResponseCookie buildLogoutRefreshTokenCookie() {
             return ResponseCookie.from(refreshCookieName, "")
                     .httpOnly(true)
-                    .secure(false)
+                    .secure(true)
                     .path("/")
                     .maxAge(0)
-                    .sameSite("Strict")
-//                    .domain(".felicitatrips.com")
+                    .sameSite("None")
+                    .domain(".felicitatrips.com")
                     .build();
         }
     }
